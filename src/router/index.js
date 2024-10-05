fdf80c0d4ab4e2c8ff893ce3abe7ccd7b229e9b1
demjhonsilver <djsilverph@gmail.com>
2024-10-05 20:42:50 +0800

    fix minor issue1

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(), // No base URL needed
+  history: createWebHashHistory('/guides/'), // No base URL needed
   routes,
 });
 
