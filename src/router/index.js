af7fb5d16568a866e12ad3a3504636ecea147575
demjhonsilver <djsilverph@gmail.com>
2024-10-05 20:53:04 +0800

    fix minor issue3

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(), // No base URL needed
+  history: createWebHashHistory('/ds-guides/'), // No base URL needed
   routes,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/guides/',
+  base: '/ds-guides/',
 })
